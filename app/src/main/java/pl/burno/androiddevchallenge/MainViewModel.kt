27b75f3c8babf2ce0eb6ada48d42d80e60bda631
burno <bruno.wieczorek@gmail.com>
2021-03-04 23:14:27 +0100

    Apply spotless changes

--- app/src/main/java/pl/burno/androiddevchallenge/MainViewModel.kt
+++ app/src/main/java/pl/burno/androiddevchallenge/MainViewModel.kt
@@ -1,17 +1,35 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package pl.burno.androiddevchallenge
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import pl.burno.androiddevchallenge.model.Time
-import pl.burno.androiddevchallenge.model.toUiTime
-import pl.burno.androiddevchallenge.ui.model.UiTime
-import pl.burno.androiddevchallenge.ui.model.UiTimePosition
-import pl.burno.androiddevchallenge.ui.model.UiTimePosition.*
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import pl.burno.androiddevchallenge.model.Time
+import pl.burno.androiddevchallenge.model.toUiTime
+import pl.burno.androiddevchallenge.ui.model.UiTime
+import pl.burno.androiddevchallenge.ui.model.UiTimePosition
+import pl.burno.androiddevchallenge.ui.model.UiTimePosition.MINUTE_TENS
+import pl.burno.androiddevchallenge.ui.model.UiTimePosition.MINUTE_UNITS
+import pl.burno.androiddevchallenge.ui.model.UiTimePosition.SECONDS_TENS
+import pl.burno.androiddevchallenge.ui.model.UiTimePosition.SECONDS_UNITS
 
 class MainViewModel : ViewModel() {
     private var currentTime = Time(minutes = 0, seconds = 0)
